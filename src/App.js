9543a2da3640c558911eabeec660eb726aa596b0
Mukkeshhari P V <mukkesh@Mukkeshs-MacBook-Air.local>
2025-07-30 17:52:32 +0530

    Add Firebase Keys

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, doc, dele
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // --- Firebase Configuration ---
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAjXaIwB-raIRzCMlbMj3AbdhVjCuanCZc",
   authDomain: "spentaf-app.firebaseapp.com",
@@ -21,11 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "515912557442",
   appId: "1:515912557442:web:d6126cd95ff51a9075259b",
   measurementId: "G-G4HJ3PX8D0"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 };
 
 // --- App Initialization ---
