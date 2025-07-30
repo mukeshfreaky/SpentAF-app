cedbb2e2f9a68d00ced6ac3178c1ad60c2348f7c
Mukkeshhari P V <mukkesh@Mukkeshs-MacBook-Air.local>
2025-07-30 19:04:24 +0530

    Downgrade Tailwind CSS to compatible versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  darkMode: 'class',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or false
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
